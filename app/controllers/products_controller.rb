e8b6691c82fbe9ebddf1f4dc3541055c09943657
denikus <denikus@list.ru>
2012-07-29 23:46:12 +0300

    create and show product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,21 @@ class ProductsController < ApplicationController
   end
 
   def new
-
     @product = Product.new
+  end
 
+  def create
+    @product = Product.new(params[:product])
+    if @product.valid?
+      @product.save!
+      redirect_to :action => "show", :id => @product.id
+    else
+      render :action => "new"
+    end
+    #redirect_to :action => "show"
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
 class Product < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :name, :status, :price, :description, :phone, :email
   STATUSES = {:used =>"БУ", :new =>"Новый"}
 end
